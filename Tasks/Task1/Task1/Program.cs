318f38d69c3e7af1b1c38a92396e35aa0b39f95b
Aka <Aka35241@gmail.com>
2020-10-08 15:58:17 +0600

    first commit

---
+++ Tasks/Task1/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 6;
+            int c = 10;
+            Console.Write($"{a}  {b}  {c}");
+        }
+
+
+    }
+
+    
+}
---
+++ Tasks/Task3/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число в см:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{a / 100}м");
+        }
+    }
+}
